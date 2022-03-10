e8c8a59b1356beec646c8984080b7c04bced5fbf
imzhangxian <imzhangxian@gmail.com>
2022-03-10 20:48:11 +0800

    Create gambler.py

---
+++ c4/gambler.py
@@ -0,0 +1,58 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+MAX_CAPITAL = 100
+P_HEAD = 0.25 # probability of head
+GAMMA = 1
+MAX_ITERS = 300
+
+values = np.zeros(100)
+policies = np.zeros(100, dtype=int)
+
+def compute_value(s, a):
+  v_new = P_HEAD * (1 if s + a >= 100 else GAMMA * values[s + a]) + \
+          (1 - P_HEAD) * (GAMMA * values[s - a] if s > a else 0)
+  return v_new
+
+# calculate value[s] for each possible actions, 
+#   find best action value pair, and 
+#   return delta of value change
+def eval_value(s):
+  v = values[s]
+  action_values = []
+  for a in range(0, s + 1):
+    # for each possible bets
+    action_values.append(compute_value(s, a))
+  if len(action_values) > 0:
+    values[s] = np.max(action_values)
+  return abs(values[s] - v)
+
+def eval_policy(s):
+  action_values = []
+  for a in range(0, s + 1):
+    # for each possible bets
+    action_values.append(compute_value(s, a))
+  if len(action_values) > 0:
+    policies[s] = np.argmax(action_values)
+    # print(policies[s], action_values)
+  return None
+
+for k in range(MAX_ITERS):
+  delta = 0
+  for s in range(1, MAX_CAPITAL):
+    delta = max(delta, eval_value(s))
+  print('Delta = ', delta, 'for iteration', k)
+  # if delta < 0.000001:
+  #   break
+
+for s in range(1, MAX_CAPITAL):
+ eval_policy(s)
+
+print(values)
+
+# eval_policy(20)
+print(policies)
+
+plt.plot(range(MAX_CAPITAL), policies)
+plt.show()
